211985b94d70ff79ab3a6d1665a67f9733daf774
alexpineda99 <74643984+alexpineda99@users.noreply.github.com>
2021-12-01 19:20:10 -0430

    Cambio de icono

--- src/Componentes/Experience.js
+++ src/Componentes/Experience.js
@@ -1,13 +1,8 @@
 import "../Css/index.css";
-import Mehey from "../Assets/img/bitmoji-20211123014412-removebg-preview.png";
-import Nav from "./Navbar";
-import PersonalInfo from "./PersonalInfo";
-import Experience from "./Experience";
 
-function Home() {
+function Experience() {
   return (
     <div className="main">
-      <Nav />
       <div className="principal">
         <div className="principal-1">
           <p className="principal-text-line1"> Hi </p>
@@ -15,14 +10,11 @@ function Home() {
           <p className="principal-text-line3"> Alex </p>
         </div>
         <div className="principal-2">
-          <img src={Mehey} alt="Greetings" className="bit-img" />
         </div>
       </div>
       <div className="second-title">Full Stack Web Developer</div>
-      <PersonalInfo/>
-      <Experience/>
     </div>
   );
 }
 
-export default Home;
+export default Experience;
